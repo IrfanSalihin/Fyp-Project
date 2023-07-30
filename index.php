c86311db2d9fcca7ef59471d71352d944bcddb86
IrfanSalihin <a179952@siswa.ukm.edu.my>
2023-07-30 15:45:38 +0800

    index

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+// Use the header() function to redirect to login.html
+header("Location: login.html");
+exit; // Make sure to include 'exit' after the header() function to prevent further execution of the script
+?>
